558cedf8eb987943fb258bb100f877c83afaa3ec
Hayden Woolstenhulme <hayd1234@gmail.com>
2022-01-15 20:28:27 -0700

    fix memory leak

--- src/Components/DashBoard/Post/ImgPost.js
+++ src/Components/DashBoard/Post/ImgPost.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import './ImgPost.css';
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
 
 export class ImgPost extends Component {
     constructor() {
         super();
         this.state = {
             imgurl: '',
-            textInput:'',
-            tagInput:'',
-            type:'img'
+            textInput: '',
+            tagInput: '',
+            type: 'img'
         }
         this.handleTagChange = this.handleTagChange.bind(this);
         this.handleTextChange = this.handleTextChange.bind(this);
@@ -18,58 +20,60 @@ export class ImgPost extends Component {
         this.sendText = this.sendText.bind(this);
     }
 
-    handleTextChange (event) {
+    handleTextChange(event) {
         this.setState({ textInput: event.target.value })
     }
 
-    handleTagChange (event) {
+    handleTagChange(event) {
         this.setState({ tagInput: event.target.value })
     }
 
-    handleUrlChange (event) {
+    handleUrlChange(event) {
         this.setState({ imgurl: event.target.value })
     }
 
-    sendText () {
-   
-        let {imgurl, textInput, type, tagInput} = this.state;
-        let {uid} = this.props.authUser;
-        axios.post('/api/posts/new', {type, tagInput, textInput, uid, imgurl}).then(() => {
-            this.setState({textInput: '', tagInput: '', imgurl: ''})
-        
+    sendText() {
+        let { imgurl, textInput, type, tagInput } = this.state;
+        let { uid } = this.props.authUser;
+        axios.post('/api/posts/new', { type, tagInput, textInput, uid, imgurl }, { cancelToken: source.token }).then(() => {
+            this.setState({ textInput: '', tagInput: '', imgurl: '' }, this.props.toggleClose)
+        }).catch(function (thrown) {
+            if (axios.isCancel(thrown)) {
+                console.log('Request canceled', thrown.message);
+            } else {
+                console.log("Error posting photo")
+            }
         })
     }
 
     render() {
-        
         return (
             <div className='img-input'>
                 <input
-                id='inputtwo'
-                value={this.state.imgurl}
-                onChange={this.handleUrlChange}
-                type='text'
-                placeholder='image url'
+                    id='inputtwo'
+                    value={this.state.imgurl}
+                    onChange={this.handleUrlChange}
+                    type='text'
+                    placeholder='image url'
                 />
-                 <textarea
-                id='inputone'
-                value={this.state.textInput}
-                onChange={this.handleTextChange}
-                type='text'
-                placeholder='Whatcha thinkin about?'
+                <textarea
+                    id='inputone'
+                    value={this.state.textInput}
+                    onChange={this.handleTextChange}
+                    type='text'
+                    placeholder='Whatcha thinkin about?'
                 />
-                 <input
-                 id='tag'
-                value={this.state.tagInput}
-                onChange={this.handleTagChange}
-                type='text'
-                placeholder='add some tags!'
+                <input
+                    id='tag'
+                    value={this.state.tagInput}
+                    onChange={this.handleTagChange}
+                    type='text'
+                    placeholder='add some tags!'
                 />
-             <div className="buttonwrapper">
-                <button onClick={()=>{this.sendText(), this.props.toggleClose()}} >Post</button>
-                <button onClick={()=>this.props.toggleClose()}>Close</button>
+                <div className="buttonwrapper">
+                    <button onClick={() => { this.sendText() }} >Post</button>
+                    <button onClick={() => this.props.toggleClose()}>Close</button>
                 </div>
-
             </div>
         )
     }
@@ -77,8 +81,6 @@ export class ImgPost extends Component {
 
 const mapStateToProps = (state) => ({
     authUser: state.sessionState.authUser
-   });
-   
-   const authCondition = (authUser) => !!authUser;
-   
-   export default connect(mapStateToProps)(ImgPost);
+});
+const authCondition = (authUser) => !!authUser;
+export default connect(mapStateToProps)(ImgPost);
--- src/Components/DashBoard/Post/TextPost.js
+++ src/Components/DashBoard/Post/TextPost.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import './TextPost.css';
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
 
 export class TextPost extends Component {
     constructor() {
@@ -10,62 +12,66 @@ export class TextPost extends Component {
             textInput: '',
             tagInput: '',
             type: 'text',
-        
+
         }
         this.handleTagChange = this.handleTagChange.bind(this);
         this.handleTextChange = this.handleTextChange.bind(this);
         this.sendText = this.sendText.bind(this);
     }
 
-    handleTextChange (event) {
+    handleTextChange(event) {
         this.setState({ textInput: event.target.value })
     }
 
-    handleTagChange (event) {
+    handleTagChange(event) {
         this.setState({ tagInput: event.target.value })
     }
 
-    sendText () {
-        let {textInput, type, tagInput} = this.state;
-        let {uid} = this.props.authUser;
-        axios.post('/api/posts/new', {textInput, type, tagInput, uid}).then(() => {
-            this.setState({textInput: '', tagInput: ''})
+    sendText() {
+        let { textInput, type, tagInput } = this.state;
+        let { uid } = this.props.authUser;
+        axios.post('/api/posts/new', { textInput, type, tagInput, uid }, { cancelToken: source.token }).then((response) => {
+            this.setState({ textInput: '', tagInput: '' }, this.props.toggleClose)
+        }).catch(function (thrown) {
+            if (axios.isCancel(thrown)) {
+                console.log('Request canceled', thrown.message);
+            } else {
+                console.log("Error posting text")
+            }
         })
     }
-    
 
-    render () {
-        
+    render() {
+
         return (
-            <div className = 'text-input'>
-
+            <div className='text-input'>
                 <textarea
-                id='inputone'
-                value={this.state.textInput}
-                onChange={this.handleTextChange}
-                type='text'
-                placeholder='Whatcha thinkin about?'
+                    id='inputone'
+                    value={this.state.textInput}
+                    onChange={this.handleTextChange}
+                    type='text'
+                    placeholder='Whatcha thinkin about?'
                 />
-                 <input
-                 id='inputtwo'
-                value={this.state.tagInput}
-                onChange={this.handleTagChange}
-                type='text'
-                placeholder='add some tags!'
+                <input
+                    id='inputtwo'
+                    value={this.state.tagInput}
+                    onChange={this.handleTagChange}
+                    type='text'
+                    placeholder='add some tags!'
                 />
                 <div className="buttonwrapper">
-                <button onClick={()=>{this.sendText(), this.props.toggleClose()}} >Post</button>
-                <button onClick={()=>this.props.toggleClose()}>Close</button>
+                    <button onClick={() => { this.sendText() }} >Post</button>
+                    <button onClick={() => this.props.toggleClose()}>Close</button>
                 </div>
-                
             </div>
         )
     }
 }
+
 const mapStateToProps = (state) => ({
     authUser: state.sessionState.authUser
-   });
-   
-   const authCondition = (authUser) => !!authUser;
-   
-   export default connect(mapStateToProps)(TextPost);
+});
+
+const authCondition = (authUser) => !!authUser;
+
+export default connect(mapStateToProps)(TextPost);
